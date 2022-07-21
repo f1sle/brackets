b82b1557256cd4d2921acc6009cc0eedb11cde15
Alex Lebedev <alexswan.dev@protonmail.ch>
2022-07-20 20:56:34 -0700

    add: 'brackets' solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = bracketsConfig.reduce((cur, next) => ({...cur, [next[0]]: next[1] }) ,{});
+  
+  let stack = [];
+  let arr = [...str];
+
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+
+    if (current !== stack[stack.length - 1]) {
+      stack.push(config[current])
+    } else if (current === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  
+  return stack.length === 0;
 }
